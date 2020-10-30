3ba3f98850f0910f22eb74c296c92f429ac5fd9c
Rory Liberty <roryliberty@gmail.com>
2020-10-30 17:11:03 -0500

    Working on the addition button function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const numbers = document.querySelectorAll('.number-buttons');
-const operators = document.querySelectorAll('.operator-buttons');
+//const operators = document.querySelectorAll('.operator-buttons');
 let displayContent = '';
 let calcType = '';
 
@@ -12,15 +12,20 @@ numbers.forEach((number) => {
     });
 });
 
-operators.forEach((operator) => {
-    operator.addEventListener('click', (e) => {
-        console.log(e.currentTarget.innerText);
-        calcType = e.currentTarget.innerText;
-    });
-});
+// operators.forEach((operator) => {
+//     operator.addEventListener('click', (e) => {
+//         console.log(e.currentTarget.innerText);
+//         calcType = e.currentTarget.innerText;
+//     });
+// });
 
-if (displayContent != '')
+// if (displayContent !== '' && calcType === '+') {
+//     addition(displayContent);
+// } else return false;
 
-function addition() {
-
-}
+ function addition(addNum) {
+    if (displayContent !== '') {
+        console.log(`First addition number: ${addNum}`);
+        console.log(`Second addition number: ${displayContent}`);
+    }   else return false;
+ }

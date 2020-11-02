a410ecb057e1b501c81cdd15928c0a6cb8c0e1ad
Rory Liberty <roryliberty@gmail.com>
2020-11-02 15:33:39 -0600

    Worked on it a bit more. Totes confused.

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
-const numbers = document.querySelectorAll('.number-buttons');
-//const operators = document.querySelectorAll('.operator-buttons');
-let displayContent = '';
-let calcType = '';
+const calculator = {
+    displayValue: '0',
+    firstNum: null,
+    waitingForSecondNum: null,
+    operator: null,
+}
 
+const keys = document.querySelector(".buttons");
 
-numbers.forEach((number) => {
-    number.addEventListener('click', (e) => {
-        console.log(e.currentTarget.innerText);
-        displayContent = e.currentTarget.innerText;
-        document.getElementById('result').innerText = displayContent;
-    });
+function updateDisplay() {
+    const display = document.querySelector('.screen');
+    display.value = calculator.displayValue;
+}
+
+function inputDigit(digit) {
+    if (calculator.displayValue === '0') {
+        calculator.displayValue = digit;
+    } else {
+        calculator.displayValue = calculator.displayValue + digit;
+    }
+}
+
+function handleOperator(newOperator) {
+    const inputValue = parseFloat(calculator.displayValue);
+
+    if (calculator.firstNum === 'null' && !isNaN(inputValue)) {
+        calculator.firstNum = inputValue;
+    }
+
+    calculator.waitingForSecondNum = true;
+    calculator.operator = newOperator;
+}
+
+function clearCalc() {
+    calculator.displayValue = '0';
+    calculator.firstNum = null;
+    calculator.waitingForSecondNum = null;
+    calculator.operator = null;
+}
+
+keys.addEventListener('click', (e) => {
+    const { target } = e;
+
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        handleOperator(target.value);
+        updateDisplay();
+        console.log(calculator.firstNum);
+        console.log(calculator.operator);
+        return;
+    }
+
+    if (target.classList.contains('digit')) {
+        inputDigit(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('clear-btn')) {
+        clearCalc();
+        updateDisplay();
+    }
 });
-
-// operators.forEach((operator) => {
-//     operator.addEventListener('click', (e) => {
-//         console.log(e.currentTarget.innerText);
-//         calcType = e.currentTarget.innerText;
-//     });
-// });
-
-// if (displayContent !== '' && calcType === '+') {
-//     addition(displayContent);
-// } else return false;
-
- function addition(addNum) {
-    if (displayContent !== '') {
-        console.log(`First addition number: ${addNum}`);
-        console.log(`Second addition number: ${displayContent}`);
-    }   else return false;
- }
